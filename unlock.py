bd4c22e02f849d1d2df91149c3492a9b603a6404
toddatctera <todd@ctera.com>
2021-01-09 01:45:51 -0500

    Added info func, removed print from unlock, added error handling.
    Removed the for loop that printed the devie details.
    It's not needed to unlock and can be made into its own task if needed.
    Created a simple info() function to print the needed info for support.
    Raise an exception if the device name entered is not found.k
    
    TODO: Re-add the logic that checks that the device is connected (maybe).

--- unlock.py
+++ unlock.py
@@ -1,29 +1,31 @@
 #!/usr/bin/python3
 # unlock.py
 # Module for ctools.py, a CTERA Portal/Edge Filer Maintenance Tool
-# Get device name, firmware, and MAC and prompt for unlock code.
-# Version 0.1 
+# Get device name, return firmware, and MAC and prompt for unlock code.
+# Version 0.2 
 from login import login
 from cterasdk import *
 import logging
 
 def unlock():
     global_admin = login()
-    print("Devices able to unlock")
-    print('Tenant | Filer Name | Firmware | MAC Address | IP Address')
-    for tenant in global_admin.portals.tenants():
-        global_admin.portals.browse(tenant.name)
-        filers = global_admin.devices.filers(include=['deviceConnectionStatus.connected'])
-        for filer in filers:
-            if filer.deviceConnectionStatus.connected:
-                firmware = filer.get('/status/device/runningFirmware')
-                mac = filer.get('/status/device/MacAddress')
-                ip = filer.get('/status/network/ports/0/ip/address')
-                print(tenant.name, filer.name, firmware, mac, ip)
     device = input("Enter device name to unlock: ")
     tenant = input("Enter tenant portal of " + device + " : ")
-    filer = global_admin.devices.device(device, tenant)
+    try:
+        filer = global_admin.devices.device(device, tenant)
+        print("Device found.")
+    except CTERAException as error:
+        logging.warning(error)
+        print("Device not found.")
+    info(filer)
     enable(filer)
+
+def info(filer):
+    mac = filer.get('/status/device/MacAddress')
+    firmware = filer.get('/status/device/runningFirmware')
+    print("Provide the following to CTERA to unlock",filer.name)
+    print(mac)
+    print(firmware)
 
 def enable(filer):
     try:
@@ -33,7 +35,7 @@ def enable(filer):
         print("Something went wrong with the prompt.")
     try:
         filer.telnet.enable(code)
-        print('Success. Telnet access unlocked')
+        print("Success. Telnet enabled on",filer.name)
     except CTERAException as error:
         logging.warning(error)
         print("Bad code or something went wrong unlocking device.")
