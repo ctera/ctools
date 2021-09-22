efd4c6e42daf59c3fecb3b1841cc81a9c1e5b2d2
Todd Butters <todd@ctera.com>
2021-09-22 17:20:15 -0400

    Check if admin has allowSSO to manage devices.
    For #26, this adds a check and logs a warning if the admin
    signing in does not have 'Allow Single Sign on to Devices'
    checked for the Read Write Admin role in the Administration tenant.

--- login.py
+++ login.py
@@ -7,8 +7,14 @@ def login(address,username,password):
         logging.info("Logging into " + address)
         global_admin = GlobalAdmin(address)
         global_admin.login(username, password)
-        logging.info("Successfully logged in to " + address)
+        logging.debug("Successfully logged in to " + address)
         global_admin.portals.browse_global_admin()
+        allow_device_sso = global_admin.get('rolesSettings/readWriteAdminSettings/allowSSO')
+        if allow_device_sso is True:
+            logging.debug('Single Sign On to Devices is allowed')
+        else:
+            logging.warning("Allow Single Sign On to Devices is not enabled. " +
+                            "Some tasks may fail or output may be incomplete")
         return global_admin
     except CTERAException as error:
         logging.warning(error)
