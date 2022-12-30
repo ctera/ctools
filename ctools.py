30d464cac6b6de6ff533809755cdeef556297d4f
kevin-ctera <kevink.ctera@gmail.com>
2022-12-29 23:26:11 -0800

    add new tools, add new status checks, move from tabs to sidebar

--- ctools.py
+++ ctools.py
@@ -10,6 +10,10 @@ from run_cmd import run_cmd
 from suspend_sync import suspend_filer_sync
 from unsuspend_sync import unsuspend_filer_sync
 from reset_password import reset_filer_password
+from cloudfs import ask,create_folders,usage
+from smb_audit import convert_size, extract_epoch, convert_time, show_ftr_details, summarize_audit, parse_audit, search_audit, smb_audit
+from pandas.api.types import CategoricalDtype
+import re
 
 
 def set_logging(p_level=logging.INFO, log_file="info-log.txt"):
@@ -29,7 +33,7 @@ def set_logging(p_level=logging.INFO, log_file="info-log.txt"):
             logging.StreamHandler()])
 
 
-@Gooey(advanced=True, navigation='TABBED', program_name="CTools", use_cmd_args=True,
+@Gooey(advanced=True, navigation='SIDEBAR', program_name="CTools", use_cmd_args=True,
        default_size=(800, 750),
        menu=[{
             'name': 'File',
@@ -70,6 +74,8 @@ def main():
                     'suspend_sync': suspend_filer_sync,
                     'unsuspend_sync': unsuspend_filer_sync,
                     'reset_password': reset_filer_password,
+                    'cloudfs': create_folders,
+                    'smb_audit': smb_audit,
                     }
     parser = GooeyParser(description='Manage CTERA Edge Filers')
     parser.add_argument('--ignore-gooey', help='Run in CLI mode')
@@ -147,21 +153,56 @@ def main():
     reset_password_parser.add_argument('user_name', help='User Name')
     reset_password_parser.add_argument('filer_password', widget='PasswordField', help=new_pw_help_text)
 
+    # cloudfs sub parser
+    cloudfs_help = "Create zones, folder groups, cloud folders"
+    cloudfs_parser = subs.add_parser('cloudfs', parents=[portal_parent_parser], help=cloudfs_help)
+    cloudfs_parser.add_argument('csv_file', widget='FileChooser', help='csv file') 
+
+    # SMB Audit subparsers
+    smb_audit_help_text = "Parse, summarize, and search Samba audit logs from CTEA devices."
+    smb_audit_parser = subs.add_parser('smb_audit', help=smb_audit_help_text)
+    smb_audit_parser.add_argument('-f', '--function', action='store', dest='function', widget='Dropdown', choices=['Parse', 'Summarize', 'Search'], required=True, default='Parse', type=str, help='Source log directory contianing ONLY audit log files (Format: share/folder1/folder2/folderN')
+    smb_audit_parser.add_argument('-sd', '--source-directory', action='store', dest='source_directory', widget='DirChooser', type=str, help='Source log directory contianing ONLY audit log files (Format: share/folder1/folder2/folderN')
+    smb_audit_parser.add_argument('-of', '--output-file', action='store', dest='output_file', type=str, help='Label for output files')
+    #smb_audit_parser.add_argument('-c', '--csv', action='store_true', dest='make_csv', default=False, help='Output a CSV file in addition to other options')    
+    smb_audit_parser.add_argument('-if', '--input-file', action='store', dest='ftr_file', type=str, widget="FileChooser", help='Name of the input file(Format: customer_location.ftr)')
+    smb_audit_parser.add_argument('-ti', '--time-interval', action='store', dest='time_interval', type=str, widget="Dropdown", choices=['5min', '10min', '15min', '20min', '30min', '60min'], required=True, default='30min', help='Time interval used for summary computations')     
+    smb_audit_parser.add_argument('-ss', '--search-string', action='store', dest='search_string', type=str, help='String to search for in the specified field.')
+    smb_audit_parser.add_argument('-sf', '--search-field', action='store', dest='search_field', type=str, widget="Dropdown", choices=['path', 'share', 'user'], default='path', help='Field to be searched from the audit logs: path, user, result, share.')  
+    smb_audit_parser.add_argument('--debug', action='store_true', dest='is_debug', default=False, help='Enables debug output of arguments')
+    smb_audit_parser.add_argument('-v', '--verbose', help='Add verbose logging', action='store_true')
+    
+    #Create dictionary of Samba operation types
+    smb_operations = ["op=ACEChanged", "op=ACLAdded", "op=ACLDeleted", "op=AclDenied", "op=chown",
+                    "op=create", "op=createDenied", "op=delete", "op=deleteDenied", "op=move",
+                    "op=open", "op=OpenDenied", "op=setattrib", "op=setdacl", "op=write"]
+    smb_operation_type = CategoricalDtype(categories=smb_operations)    
+    smb_ops_group = smb_audit_parser.add_argument_group('Show SMB Operations')
+    for op in smb_operations:
+        op_string = re.sub('op=', '', op)
+        op_command = "--" + op_string
+        smb_ops_group.add_argument(op_command, action='store_false', default=True, widget='CheckBox', help=op_string )
+
+    
     # Parse arguments and run commands of chosen task
     args = parser.parse_args()
     if args.verbose:
         set_logging(logging.DEBUG, 'debug-log.txt')
     else:
         set_logging()
+
+        
     # Uncomment to log the arguments. Will reveal a GUI password in plain text.
     # logging.debug(args)
     logging.info('Starting ctools')
     # For CLI, if required password arg is a ?, prompt for password
-    if args.password == '?':
-        args.password = getpass(prompt='Password: ')
-    # Create a global_admin object and login.
+    if args.task != 'smb_audit':
+        if args.password == '?':
+            args.password = getpass(prompt='Password: ')
+        # Create a global_admin object and login.
     # In the future, if we add device login tasks, we'll need to change this.
-    global_admin = global_admin_login(args.address, args.username, args.password, args.ignore_cert)
+    if args.task != 'smb_audit':  #added this since smb_audit does not require portal login
+        global_admin = global_admin_login(args.address, args.username, args.password, args.ignore_cert)
     # Set the chosen task.
     selected_task = FUNCTION_MAP[args.task]
     # Run selected task with required sub arguments.
@@ -181,9 +222,14 @@ def main():
         selected_task(global_admin, args.device_name, args.tenant_name)
     elif args.task == 'reset_password':
         selected_task(global_admin, args.device_name, args.tenant_name, args.user_name, args.filer_password)
+    elif args.task == 'cloudfs':
+        selected_task(global_admin, args.csv_file)
+    elif args.task == 'smb_audit':
+        selected_task(args)
     else:
         logging.error('No task found or selected.')
-    global_admin.logout()
+    if args.task != 'smb_audit':  #added this since smb_audit does not require portal login
+        global_admin.logout()
     logging.info('Exiting ctools')
 
 
